3b74fa073fb7fe1680885d40ab2c2353818090b4
adhi <deviabhishek.devi@.com>
2021-10-28 17:17:41 +0530

    variable

---
+++ variables.sh
@@ -0,0 +1,24 @@
+##if we assain a name to a set of data,then that is variable
+
+#syntax: VAR=DATA
+
+
+#number
+a=100
+#string
+b=abc
+
+# in bash shell and also by default there are no data types, shell consider e everything as string
+#access the data in shell using "$"or "${}"
+
+
+echo value of a=$a
+echo value of b=$b
+
+#date=28-10-21
+date=$(date +%f)
+
+a=(10 20)
+a=10
+export a
+
